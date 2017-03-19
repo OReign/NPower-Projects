b2cbbfed9bb21bb14bc096516c27013a5e53df65
OReign <otswaby@gmail.com>
2017-03-19 11:14:31 -0400

    Adding Polymorphism Demon code worked on in class

---
+++ PolymorphismDemo/PolymorphismDemo/Circle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismDemo
+{
+    class Circle : Shape 
+    {
+        public override void Draw() { 
+            Console.WriteLine("Drawing a Circle");
+            base.Draw();
+
+
+    }
+    }
+}
